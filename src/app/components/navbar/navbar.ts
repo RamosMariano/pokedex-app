c7db2e03992a6cfaaaa0bb5f70477499a728586a
Klaa5 <nic.n@outlook.es>
2026-06-03 20:36:45 -0300

    Agregado link a perfil de usuario con imagen stock

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -12,4 +12,27 @@ import { RouterModule } from '@angular/router';
 export class NavbarComponent 
 {
   logueado = false; //para evitar mostrar otros links si no hay sesion
+  profileImg = this.asignarImagenLocalStorage();
+
+  selectorRuta() //te manda al login si no estas logueado.
+  {
+    if (this.logueado === true) 
+    {
+      return "/home";
+    }
+
+    return "/login";
+  }
+
+  stockImg()
+  {
+    this.profileImg = "/assets/img/usrProfile.jpeg";
+  }
+
+  asignarImagenLocalStorage()
+  {
+    //pendiente.
+    return "/assets/img/usrProfile.jpeg";
+  }
+
 }
