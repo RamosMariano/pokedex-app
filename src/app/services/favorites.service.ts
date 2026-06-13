34d0739af9160c4a0a7e4f33a0e273b6fbda63f7
Klaa5 <nic.n@outlook.es>
2026-06-12 23:02:51 -0300

    Version primaria de clase servicio favoritos implementada

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -3,4 +3,76 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class FavoritesService {}
+
+export class FavoritesService 
+{
+
+
+  obtenerPokeFavoritos() 
+  {
+    let listaFavoritos = localStorage.getItem('pokemonFavoritos');
+
+    if(listaFavoritos === null) 
+    {
+      return [];
+    }
+
+    return JSON.parse(listaFavoritos);
+
+  }
+
+  agregarPoke(pokemon: any) 
+  {
+
+    let listaFavoritos = this.obtenerPokeFavoritos();
+    let yaEsFavorito = this.esFavoritoSN(pokemon.name);
+
+    if(yaEsFavorito === false)
+    {
+      listaFavoritos.push(pokemon);
+      localStorage.setItem('pokemonFavoritos',JSON.stringify(listaFavoritos));
+    }
+
+  }
+
+  esFavoritoSN(nombre: string)
+  {
+    let listaFavoritos = this.obtenerPokeFavoritos();
+
+    for (const pokemon of listaFavoritos)
+    {
+      if (pokemon.name === nombre)
+      {
+          return true;
+      }
+    }
+
+    return false;
+
+  }
+
+  eliminarFavorito(nombre: string) 
+  {
+    let listaFavoritos = this.obtenerPokeFavoritos();
+
+    let temp = [];
+
+    for(let poke of listaFavoritos)
+    {
+      if(poke.name !== nombre)
+      {
+        temp.push(poke);
+      }
+    }
+
+    localStorage.setItem('pokemonFavoritos',JSON.stringify(temp));
+    
+  }
+
+  resetearTodo() 
+  {
+    localStorage.removeItem('pokemonFavoritos');
+  }
+}
+
+ 
