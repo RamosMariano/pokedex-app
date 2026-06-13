151d6fbb5ac268896be0ab4bce40b7e7457a2f91
Klaa5 <nic.n@outlook.es>
2026-06-13 00:02:59 -0300

    TS con carga de pokemon favs desde LS onInit

--- src/app/pages/favorites/favorites.ts
+++ src/app/pages/favorites/favorites.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FavoritesService } from '../../services/favorites.service';
+import { PokemonCardComponent } from '../../components/pokemon-card/pokemon-card';
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PokemonCardComponent],
   templateUrl: './favorites.html',
   styleUrl: './favorites.scss'
 })
-export class FavoritesComponent {}
+export class FavoritesComponent 
+{
+  constructor(private favouritesService:FavoritesService) {}
+
+  pokeFavs: any = [];
+
+  ngOnInit()
+  {
+    this.pokeFavs = this.favouritesService.obtenerPokeFavoritos();
+  }
+
+}
