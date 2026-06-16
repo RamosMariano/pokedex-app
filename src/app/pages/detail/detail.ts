ddb941046bcc6c4cb2f4439d9ca5ae68c696eef4
Frankb-eng <franciscoblazevic@hotmail.com>
2026-06-16 18:47:34 -0300

    Terminado el grafico de Hexagono para los stats

--- src/app/pages/detail/detail.ts
+++ src/app/pages/detail/detail.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PokemonService } from '../../services/pokemon.service';
 import { SpinnerComponent } from '../../components/spinner/spinner';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-detail',
@@ -49,12 +50,79 @@ export class DetailComponent {
       value: s.base_stat,
       percentage: Math.round((s.base_stat / 255) * 100)
     }));
+
+    /*const data = {
+      labels: [
+        'Hp',
+        'ATK',
+        'DEF',
+        'Sp.ATK',
+        'Sp.DEF',
+        'Spe'
+      ],
+      datasets: [{
+        label: this.pokemon.name,
+        data: this.pokemon.stats.map((s: any) => s.base_stat),
+        fill: true,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgb(255, 99, 132)',
+        pointBackgroundColor: 'rgb(255, 99, 132)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(255, 99, 132)'
+      }]
+    };
+    const config = {
+      type: 'radar',
+      data: data,
+      options: {
+        elements: {
+          line: {
+            borderWidth: 3
+          }
+        }
+      },
+    };
+    
+    new Chart(
+      document.getElementById('radarChart') as HTMLCanvasElement, config as any
+    );*/
+
   } catch (e) {
     console.error('error:', e);
     this.router.navigate(['/home']);
   } finally {
     this.loading = false;
     this.cdr.detectChanges();
+
+    //Hexagono
+    console.log(this.pokemon.stats);
+    const radarData = this.pokemon.stats.map(
+      (s: any) => s.base_stat
+    );
+
+    new Chart(
+      document.getElementById('radarChart') as HTMLCanvasElement,
+      {
+        type: 'radar',
+        data: {
+          labels: ['HP', 'ATK', 'DEF', 'SP.ATK', 'SP.DEF', 'SPEED'],
+          datasets: [{
+            label: this.pokemon.name,
+            data: radarData,
+            fill: true
+          }]
+        },
+        options: {
+          scales: {
+            r: {
+              min: 0,
+              max: 255
+            }
+          }
+        }
+      }
+    );
   }
 }
 
