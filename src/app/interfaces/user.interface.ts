632d5128e34c28dd9eae57340f52bbab2e811c0e
RamosMariano <mariano.ramos@estudiantes.utec.edu.uy>
2026-06-18 02:24:46 -0300

    arreglado UserService adaptado a Keycloak, lee kc_user y kc_token

--- src/app/interfaces/user.interface.ts
+++ src/app/interfaces/user.interface.ts
@@ -1,9 +1,8 @@
 export interface User {
-
-    username: string;
-    password: string;
-    rol: 'usuario' | 'admin';
-    nombreVisible?: string;
-    ciudad?: string;
-    fotoPerfil?: string;
+  username: string;
+  password: string;
+  rol: 'usuario' | 'admin';
+  nombreVisible?: string;
+  ciudad?: string;
+  fotoPerfil?: string;
 }
